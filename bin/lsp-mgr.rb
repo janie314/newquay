074258cd5aebeefd41807b2e0f1aa3a868b4d8b8
12ball <pub@janie.page>
2023-12-18 20:13:30 -0600

    lsp-mgr install cargo works

--- bin/lsp-mgr.rb
+++ bin/lsp-mgr.rb
@@ -1,4 +1,8 @@
 require "thor"
+require_relative "../lib/lsp_servers/rust"
+require "semantic_logger"
+
+SemanticLogger.add_appender(io: $stdout, formatter: :color)
 
 class App < Thor
   package_name "LspMgr"
@@ -9,7 +13,11 @@ class App < Thor
 
   desc "install [LSP_NAME]", "install an lsp"
   def install(name)
-    puts name
+    if name =~ /rust/i || name =~ /cargo/i
+      Rust.install
+    else
+      puts "well i don't know what #{name} is..."
+    end
   end
 end
 
--- lib/lsp_servers/rust.rb
+++ lib/lsp_servers/rust.rb
@@ -1,15 +1,22 @@
+require_relative "../helpers"
+require "semantic_logger"
+
 module Rust
   def self.l
     @l ||= SemanticLogger["rust"]
   end
 
-  def install
-    l.info "checking if Rust installed..."
-    return false unless (command? ("cargo") && command? ("rustup"))
-    l.info "installing rust-src..."
-    return false unless (system "rustup", "component", "add", "rust-src")
-    l.info "installing rust-analyzer..."
-    return false unless (system "rustup", "component", "add", "rust-analyzer")
+  def self.dependencies
+    l.info "installing rust toolchain..."
+    system "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+  end
 
+  def self.install
+    l.info "checking if Rust installed and installing if not..."
+    dependencies unless (command? "cargo") && (command? "rustup")
+    l.info "installing rust-src..."
+    return false unless system "rustup", "component", "add", "rust-src"
+    l.info "installing rust-analyzer..."
+    false unless system "rustup", "component", "add", "rust-analyzer"
   end
 end
