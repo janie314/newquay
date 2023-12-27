c22286ec7d542fac8cc80f7c26b6f38d329cb4ea
12ball <pub@janie.page>
2023-12-27 09:06:32 -0600

    Working neotree/session

--- lua/config/neo-tree.lua
+++ lua/config/neo-tree.lua
@@ -1,37 +1,42 @@
 local neotree = require "neo-tree"
 
 neotree.setup {
-	use_libuv_file_watcher = true,
-	filesystem = {
-		filtered_items = {
-			visible = true,
-			hide_dotfiles = false,
-			hide_gitignored = true,
-		},
-	},
-	window = {
-		width = 30,
-		close_if_last_window = true,
-		enable_git_status = true,
-		-- winbar = false,
-		enable_diagnostics = true,
-		mappings = {
-			["a"] = {
-				"add",
-				config = {
-					show_path = "none", -- "none", "relative", "absolute"
-				},
-			},
-			["A"] = "add_directory",
-			["d"] = "delete",
-			["r"] = "rename",
-		},
-		mapping_options = { noremap = true, nowait = true },
-	},
+    use_libuv_file_watcher = true,
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+        },
+    },
+    window = {
+        width = 30,
+        close_if_last_window = true,
+        enable_git_status = true,
+        -- winbar = false,
+        enable_diagnostics = true,
+        mappings = {
+            ["a"] = {
+                "add",
+                config = {
+                    show_path = "none", -- "none", "relative", "absolute"
+                },
+            },
+            ["A"] = "add_directory",
+            ["d"] = "delete",
+            ["r"] = "rename",
+        },
+        mapping_options = { noremap = true, nowait = true },
+    },
 }
 
-require('session_manager').setup {
-	autosave_ignore_filetypes = { 'neo-tree' }
+require("session_manager").setup {
+    autosave_ignore_filetypes = { "neo-tree", "gitcommit", "gitrebase" },
 }
 
-require("neo-tree.command").execute { toggle = true, position = "left" }
+local config_group = vim.api.nvim_create_augroup("MyConfigGroup", {}) -- A global group for all your config autocommands
+vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "SessionLoadPost",
+    group = config_group,
+    callback = function() require("neo-tree.command").execute { toggle = true, position = "left" } end,
+})
