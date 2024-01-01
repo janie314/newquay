6f4ef24e8bc5405a1ba00bcd5de280dbe0d32922
12ball <pub@janie.page>
2023-12-31 18:54:45 -0600

    1704070485

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,70 +6,70 @@ local lspfmt = require "lsp-format"
 
 -- diagnostics
 require("trouble").setup {
-  position = "right",
-  action_keys = {
-    toggle_mode = "m",
-    preview = "p",
-    next = "j",
-  },
-  multiline = true,
-  indent_lines = true,
-  win_config = { border = "single" },
+	position = "right",
+	action_keys = {
+		toggle_mode = "m",
+		preview = "p",
+		next = "j",
+	},
+	multiline = true,
+	indent_lines = true,
+	win_config = { border = "single" },
 }
 
 -- completions
 local cmp = require "cmp"
 cmp.setup {
-  snippet = {
-    expand = function(args) require("luasnip").lsp_expand(args.body) end,
-  },
-  sources = { { name = "nvim_lsp" } },
-  mapping = cmp.mapping.preset.insert {
-    ["j"] = cmp.mapping.select_next_item(),
-    ["k"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.abort(),
-    ["<Tab>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
+	snippet = {
+		expand = function(args) require("luasnip").lsp_expand(args.body) end,
+	},
+	sources = { { name = "nvim_lsp" } },
+	mapping = cmp.mapping.preset.insert {
+		["<A-j>"] = cmp.mapping.select_next_item(),
+		["<A-k>"] = cmp.mapping.select_prev_item(),
+		["<CR>"] = cmp.mapping.abort(),
+		["<Tab>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	},
 }
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- deno
 lspconfig.denols.setup {
-  on_attach = lspfmt.on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "jsonc", "markdown" },
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "package.json"),
+	on_attach = lspfmt.on_attach,
+	capabilities = capabilities,
+	single_file_support = true,
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "jsonc", "markdown" },
+	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "package.json"),
 }
 
 -- golang
 lspconfig.gopls.setup {
-  on_attach = lspfmt.on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
+	on_attach = lspfmt.on_attach,
+	capabilities = capabilities,
+	single_file_support = true,
 }
 
 -- ruby
 lspconfig.solargraph.setup {
-  cmd = { "bundle", "exec", "solargraph", "stdio" },
-  on_attach = lspfmt.on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
-  init_options = { formatting = true },
-  settings = { solargraph = { diagnostics = true } },
+	cmd = { "bundle", "exec", "solargraph", "stdio" },
+	on_attach = lspfmt.on_attach,
+	capabilities = capabilities,
+	single_file_support = true,
+	init_options = { formatting = true },
+	settings = { solargraph = { diagnostics = true } },
 }
 
 -- lua
 lspconfig.lua_ls.setup {
-  on_attach = lspfmt.on_attach,
-  capabilities = capabilities,
-  single_file_support = true,
-  settings = {
-    Lua = {
-      completion = { enable = true, callSnippet = "Replace" },
-      format = { enable = true },
-    },
-  },
+	on_attach = lspfmt.on_attach,
+	capabilities = capabilities,
+	single_file_support = true,
+	settings = {
+		Lua = {
+			completion = { enable = true, callSnippet = "Replace" },
+			format = { enable = true },
+		},
+	},
 }
 
 -- rust
@@ -85,24 +85,25 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "<space>d", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<space>i", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<space>s", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<space>mv", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<space>r", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format { async = true } end, opts)
-  end,
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "<space>d", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<space>i", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<space>s", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+			opts)
+		-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>mv", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<space>r", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format { async = true } end, opts)
+	end,
 })
